59286adca53da0c58f86b0d5a6c5e73d6b6cd8a3
Yoan <yoanribot@gmail.com>
2022-01-21 23:38:46 +0100

    Implement a simple scrapper using scraperapi

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,68 @@
 import express from 'express';
+import request from 'request-promise';
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+const apiKey = process.env.SCRAPPER_API_KEY
+const baseUrl = `http://api.scraperapi.com?api_key=${apiKey}&autoparse=true`
 
 app.use(express.json());
 
 app.get('/', (_, res) => {
   res.send("Welcome to Amazon Scrapper API");
+});
+
+app.get('/products/:productId', async (req, res) => {
+  const { productId } = req.params;
+
+  try {
+    const response = await request(`${baseUrl}&url=https://www.amazon.fr/dp/${productId}`);
+
+    res.json(JSON.parse(response));
+  } catch (error) {
+    console.log({ error });
+    res.json(error);
+  }
+});
+
+app.get('/products/:productId/reviews', async (req, res) => {
+  const { productId } = req.params;
+
+  try {
+      const response = await request(`${baseUrl}&url=https://www.amazon.com/product-reviews/${productId}`);
+
+      res.json(JSON.parse(response));
+  } catch (error) {
+    console.log({ error });
+
+    res.json(error);
+  }
+});
+
+app.get('/products/:productId/offers', async (req, res) => {
+  const { productId } = req.params;
+
+  try {
+      const response = await request(`${baseUrl}&url=https://www.amazon.com/gp/offer-listing/${productId}`);
+
+      res.json(JSON.parse(response));
+  } catch (error) {
+      res.json(error);
+  }
+});
+
+app.get('/search/:searchQuery', async (req, res) => {
+  const { searchQuery } = req.params;
+
+  try {
+      const response = await request(`${baseUrl}&url=https://www.amazon.com/s?k=${searchQuery}`);
+
+      res.json(JSON.parse(response));
+  } catch (error) {
+      res.json(error);
+  }
 });
 
 app.listen(PORT, () => {
